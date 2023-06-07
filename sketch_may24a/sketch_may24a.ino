209cd9c135feca04ee0756a0fa63bdc2e466d086
Ulrich Seemann <95617275+fritze0221@users.noreply.github.com>
2023-06-07 19:28:37 +0200

    Add files via upload

--- sketch_may24a/sketch_may24a.ino
+++ sketch_may24a/sketch_may24a.ino
@@ -1,94 +1,169 @@
-#include "Arduino_BHY2.h"
+//Tim Herrigt, Ulrich Seemann
 
-#define THRESHOLD_GAS 500
-#define THRESHOLD_TEMP 0.1
-#define THRESHOLD_PRESSURE 0.02
+#include "Arduino_BHY2.h"
+#include "Nicla_System.h"
+
+#define THRESHOLD_GAS 600
+#define THRESHOLD_TEMP 0.3
+#define THRESHOLD_PRESSURE 0.05
 
 Sensor pressure(SENSOR_ID_BARO);
-Sensor step(SENSOR_ID_STD);
-Sensor mag(SENSOR_ID_MAG);
+Sensor step(SENSOR_ID_STC);
 Sensor airq(SENSOR_ID_HUM);
 Sensor airbsec(SENSOR_ID_BSEC);
 Sensor gas(SENSOR_ID_GAS);
 Sensor temp(SENSOR_ID_TEMP);
 
-
-
 void setup() {
   Serial.begin(115200);
   BHY2.begin();
 
+  nicla::begin();
+  nicla::leds.begin();
+
   pressure.begin();
   step.begin();
-  mag.begin();
   airq.begin();
   airbsec.begin();
   gas.begin();
   temp.begin();
-  delay(4000);
+
+  delay(8000);
+
+  Serial.println("Start: ");
 }
 
-long check_timestamp = 1000;
-long millis_check = 0;
-bool start_timestamp = true;
+long check_timestamp_es = 2000;
+long millis_check_es = 0;
+bool start_timestamp_es = true;
 
-float gasquality_start;
-float temperature_start;
+float pressure_start_es = 0;
+int step_start_es = 0;
 
-bool gas_inside = true;
-bool temp_inside = true; 
+float delta_pressure_old_es = 0; 
+int delta_steps_old_es = 0;
 
-int InsideOutside(){
+bool pressure_continuity_check_es = false; 
+bool steps_continuity_check_es = false;
 
-  if(start_timestamp){
-    gasquality_start = float(gas.value());
-    temperature_start = float(temp.value());
+bool step_check_es = false;
+bool elev_check_es = false;
 
-    millis_check = millis();
-    start_timestamp = false;
+int ElevatorStepps(){
+
+  if(start_timestamp_es){
+    pressure_start_es = float(pressure.value());
+    step_start_es = int(step.value());
+
+    millis_check_es = millis();
+    start_timestamp_es = false;
   }
 
-  if(millis() - millis_check > check_timestamp){
-    float delta_gasquality = float(gas.value()) - gasquality_start;
-    float delta_tempquality = float(temp.value()) - temperature_start;
+  if((millis() - millis_check_es) > check_timestamp_es){
+    float delta_pressure = float(pressure.value()) - pressure_start_es;
+    int delta_steps = int(step.value()) - step_start_es;
 
-    Serial.println(String(delta_gasquality));
-    Serial.println(String(delta_tempquality));
-    //Serial.println(String(gasquality_start));
-    //Serial.println(String(float(gas.value())));
-
-    if((abs(delta_gasquality) > (float) THRESHOLD_GAS) && delta_gasquality > 0.0){
-      gas_inside = false;
+    if((abs(delta_pressure) <= (abs(delta_pressure_old_es) + THRESHOLD_PRESSURE)) && (abs(delta_pressure) >= (abs(delta_pressure_old_es) - THRESHOLD_PRESSURE) && (abs(delta_pressure) > (THRESHOLD_PRESSURE)))){
+      pressure_continuity_check_es = true; 
+    }
+    else{
+      pressure_continuity_check_es = false;
+    }
+    
+    if((int)step.value() > step_start_es){
+      steps_continuity_check_es = true; 
+    }
+    else{
+      steps_continuity_check_es = false;
     }
 
-    if((abs(delta_gasquality) > (float) THRESHOLD_GAS) && delta_gasquality < 0.0){
-      gas_inside = true;
+    if(pressure_continuity_check_es && steps_continuity_check_es){
+      step_check_es = true;
+      elev_check_es = false;
     }
 
-    if((abs(delta_tempquality) > (float) THRESHOLD_TEMP) && delta_tempquality > 0.0){
-      temp_inside = false;
+    if(pressure_continuity_check_es && !steps_continuity_check_es){
+      step_check_es = false;
+      elev_check_es = true;
     }
 
-    if((abs(delta_tempquality) > (float) THRESHOLD_TEMP) && delta_tempquality < 0.0){
-      temp_inside = true;
+    if(!pressure_continuity_check_es){
+      step_check_es = false;
+      elev_check_es = false;
     }
-   
-    start_timestamp = true;
-   
-    if(gas_inside && temp_inside) return 2;
-    if(gas_inside && !temp_inside) return 1;
-    if(!gas_inside && temp_inside) return -1;
-    if(!gas_inside && !temp_inside) return -2;
+
+    start_timestamp_es = true;
+    delta_pressure_old_es = delta_pressure;
+    delta_steps_old_es = delta_steps;
+
+    if(step_check_es && !elev_check_es) return 1; // Treppe
+    if(!step_check_es && elev_check_es) return -1; // Fahrstuhl
+    if(!step_check_es && !elev_check_es) return 0;
   }
   return 0;
 }
 
+long check_timestamp_io = 1000;
+long millis_check_io = 0;
+bool start_timestamp_io = true;
 
+float gasquality_start_io = 0;
+float temperature_start_io = 0;
 
+bool gas_inside_io = true;
+bool temp_inside_io = true; 
+
+int InsideOutside(){
+
+  if(start_timestamp_io){
+    gasquality_start_io = float(gas.value());
+    temperature_start_io = float(temp.value());
+
+    millis_check_io = millis();
+    start_timestamp_io = false;
+  }
+
+  if(millis() - millis_check_io > check_timestamp_io){
+    float delta_gasquality = float(gas.value()) - gasquality_start_io;
+    float delta_tempquality = float(temp.value()) - temperature_start_io;
+
+    if((abs(delta_gasquality) > (float) THRESHOLD_GAS) && delta_gasquality > 0.0){
+      gas_inside_io = false;
+    }
+
+    if((abs(delta_gasquality) > (float) THRESHOLD_GAS) && delta_gasquality < 0.0){
+      gas_inside_io = true;
+    }
+
+    if((abs(delta_tempquality) > (float) THRESHOLD_TEMP) && delta_tempquality > 0.0){
+      temp_inside_io = false;
+    }
+
+    if((abs(delta_tempquality) > (float) THRESHOLD_TEMP) && delta_tempquality < 0.0){
+      temp_inside_io = true;
+    }
+
+    start_timestamp_io = true;
+
+    if(gas_inside_io && temp_inside_io) return 2; //Drinnen
+    if(gas_inside_io && !temp_inside_io) return 1;
+    if(!gas_inside_io && temp_inside_io) return -1;
+    if(!gas_inside_io && !temp_inside_io) return -2; //Draußen
+  }
+
+  return 0;
+}
 
 void loop(){
   BHY2.update();
   // Check sensor values every second  
   int innout = InsideOutside();
-  if(innout != 0) Serial.println((int) innout);
-}
+  int elevstep = ElevatorStepps();
+
+  if(innout == 2 || innout == 1) nicla::leds.setColor(green);  //Drinnen
+  if(innout == -2 || innout == -1) nicla::leds.setColor(blue); //Drau0en 
+
+  if(elevstep == 1) nicla::leds.setColor(red); //Treppe
+  if(elevstep == -1) nicla::leds.setColor(yellow); //Fahrstuhl
+
+  }
